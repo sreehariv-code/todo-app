4aec345cf439686eea13211df8020c3a575e4cb2
SREEHARI P V <sreehariv07@gmail.com>
2022-12-24 21:17:25 +0530

    near to completion, working on useEffect problem

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {v4 as uuidv4} from 'uuid'
 
-const Form = ({inputText,setInputText,todos,setTodos}) => {
+const Form = ({inputText,setInputText,todos,setTodos,setStatus}) => {
     const inputTextHandler = ( e ) => {
          setInputText( e.target.value)
     }
@@ -16,6 +16,12 @@ const Form = ({inputText,setInputText,todos,setTodos}) => {
         ] );
         setInputText( "" );
     }
+
+    const statusHandler = (e) => {
+       setStatus( e.target.value );
+    }
+
+
   return (
       <form>
           <input value={inputText} onChange={inputTextHandler} type="text" className="todo-input" />
@@ -23,7 +29,7 @@ const Form = ({inputText,setInputText,todos,setTodos}) => {
             <i className='fas fa-plus-square'></i>
           </button>
           <div className='select'>
-              <select name='todos' className='filter-todo'>
+              <select onChange={statusHandler} name='todos' className='filter-todo'>
                   <option value="all">All</option>
                   <option value="completed">Completed</option>
                   <option value="uncompleted">Uncompleted</option>
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import Todo from './Todo'
-const TodoList = ({todos,setTodos}) => {
+const TodoList = ({todos,setTodos,filteredTodos}) => {
   return (
       <div className="todo-container">
           <ul className='todo-list'>
-              { todos && todos.map( ( todo) => (
-                  <Todo todos={ todos } setTodos={ setTodos }  key={todo.id} text={ todo.text} />
+              { filteredTodos && filteredTodos.map( ( todo) => (
+                  <Todo todos={ todos } setTodos={ setTodos } todo={todo} key={todo.id} text={ todo.text} />
               ))}
           </ul>
       </div>
